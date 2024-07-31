c7e35d96959a8da37f4649f2c57b8a06e9421010
Snehangshu Biswas <dev.snehangshu@gmail.com>
2024-08-01 00:30:17 +0530

    [feature]: updated calculateExpression

--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -1,9 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CalcDto } from './calc.dto';
 
 @Injectable()
 export class CalcService {
   calculateExpression(calcBody: CalcDto) {
-    return 0;
+    if (!calcBody.expression || calcBody.expression.length === 0) {
+      return 0;
+    }
+
+    const stack = [];
+
+    let current = 0;
+    let operation = '+';
+
+    let lastVisited = '';
+
+    const charArrayExpression = calcBody.expression.split('');
+
+    for (let i = 0; i < charArrayExpression.length; i++) {
+      // check if last two visited characters are non digits
+      if (lastVisited !== '') {
+        if (isNaN(+lastVisited) && isNaN(+charArrayExpression[i])) {
+          throw new BadRequestException('Invalid expression provided');
+        }
+      }
+
+      // check if the last character is a non digit
+      if (
+        i === charArrayExpression.length - 1 &&
+        isNaN(+charArrayExpression[i])
+      ) {
+        throw new BadRequestException('Invalid expression provided');
+      }
+
+      // update the last visited character
+      lastVisited = charArrayExpression[i];
+
+      if (!isNaN(+charArrayExpression[i])) {
+        current = current * 10 + +charArrayExpression[i] - 0;
+      }
+
+      if (
+        isNaN(+charArrayExpression[i]) ||
+        i === charArrayExpression.length - 1
+      ) {
+        if (operation === '+') {
+          stack.push(current);
+        } else if (operation === '-') {
+          stack.push(-current);
+        } else if (operation === '*') {
+          stack.push(stack.pop() * current);
+        } else if (operation === '/') {
+          stack.push(stack.pop() / current);
+        } else {
+          throw new BadRequestException('Invalid expression provided');
+        }
+        operation = charArrayExpression[i];
+        current = 0;
+      }
+    }
+
+    let result = 0;
+
+    while (stack.length !== 0) {
+      result += stack.pop();
+    }
+
+    return result;
   }
 }
